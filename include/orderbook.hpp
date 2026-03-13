53bbb757f439e36b91f71d24bab9641eacb362fa
maxxfuu <maxfu826@gmail.com>
2026-03-13 02:03:07 -0700

    implementation of the addOrder

--- include/orderbook.hpp
+++ include/orderbook.hpp
@@ -12,23 +12,41 @@ struct PriceLevel {
   Trading::PRICE price;
   Trading::QUANTITY quantity;
   std::list<Order> orders;
+  PriceLevel(Trading::PRICE p) : price(p), quantity(0){}
 };
 
 class OrderBook {
   public:
     void handleOrder(Order& order);
-
     void cancelOrder(int64_t id);
+
+    Trading::PRICE getBestBid(const Order& order) {
+      return bids.empty() ? 0 : bids.begin()->first;
+    }
+
+    Trading::PRICE getBestAsk(const Order& order) {
+      return asks.empty() ? 0 : asks.begin()->first; 
+    }
 
   private:
     std::map<Trading::PRICE, PriceLevel, std::greater<Trading::PRICE>> bids;
-    std::map<Trading::PRICE, PriceLevel> ask;
-
-    std::unordered_map<uint64_t, std::list<Order>::iterator> order_lookup;
+    std::map<Trading::PRICE, PriceLevel> asks;
 
     // top of book 
     PriceLevel* best_bid = nullptr;
     PriceLevel* best_ask = nullptr;
+
+    // iter holds the location of the node within the PriceLevel Doubly Linkedlist
+    struct OrderLocation {
+      Trading::PRICE price;
+      Side side; 
+      std::list<Order>::iterator iter;
+    };
+
+    std::unordered_map<uint64_t, OrderLocation> order_lookup;
+    // internal helper functions after dispatch
+    void addOrder(Order& order);
+
 };
 
 #endif
--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -1,8 +1,39 @@
 #include "orderbook.hpp"
 
 void OrderBook::handleOrder(Order& order) {
-
+  // for simplicity, assume all orders handled by LOB are all Limit type 
+  if (order.type == OrderType::LIMIT) {
+    addOrder(order);
+  }
 }
 
-void cancelOrder(int64_t id) {}
+void OrderBook::addOrder(Order& order) {
+  if (order.side == Side::BUY) {
+    auto [level_it, inserted] = bids.try_emplace(order.price, order.price);
+    auto& level = level_it->second;
+
+    level.orders.push_back(order);
+    level.quantity += order.quantity;
+
+    auto order_it = level.orders.end();
+    --order_it;
+    order_lookup[order.id] = OrderLocation{order.price, order.side, order_it};
+    best_bid = &bids.begin()->second;
+  } else {
+    auto [level_it, inserted] = asks.try_emplace(order.price, order.price);
+    auto& level = level_it->second;
+
+    level.orders.push_back(order);
+    level.quantity += order.quantity;
+
+    auto order_it = level.orders.end();
+    --order_it;
+    order_lookup[order.id] = OrderLocation{order.price, order.side, order_it};
+    best_ask = &asks.begin()->second;
+  }
+}
+
+void OrderBook::cancelOrder(int64_t id) {
+  
+}
 
