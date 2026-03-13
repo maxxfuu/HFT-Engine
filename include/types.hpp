4384b55ad75ef3a2dc370daa4c50346759849806
maxxfuu <maxfu826@gmail.com>
2026-03-12 23:04:33 -0700

    implemented a fixed point engine and a cache aligned struct. working on orderbook now

--- include/types.hpp
+++ include/types.hpp
@@ -2,21 +2,53 @@
 #include <cstdint>
 #include <variant>
 
-enum class Side {
-  BUY,
-  SELL,
-};
+namespace Trading {
+  using PRICE = int64_t;
+  using TIMESTAMP = uint64_t;
+  using QUANTITY = uint32_t;
+  using SYMBOL_ID = uint32_t;
 
-enum class OrderType {
-  LIMIT,
-  MARKET,
-};
+  constexpr PRICE SCALE = 100'000'000;
 
-struct Order {
+  // Used when coverting a double price to a fixed price
+  inline PRICE ToFixed(double price) {
+    return static_cast<PRICE>(price * SCALE + 0.5);
+  }
+}
+
+enum class Side : uint8_t { BUY, SELL };
+enum class OrderType : uint8_t { LIMIT, MARKET };
+
+struct alignas(64) Order {
+  // 8 bytes
   uint64_t id;
-  double price;
-  uint32_t quantity;
+  Trading::PRICE price;
+  Trading::TIMESTAMP timestamp;
+
+  // 4 bytes
+  Trading::QUANTITY quantity;
+  Trading::SYMBOL_ID symbol_id;
+
+  // 1 byte
   Side side;
   OrderType type;
-};
+  
+  // Automated padding to ensure 64 bytes cache line alignment
+  char padding[64 - sizeof(uint64_t) - sizeof(Trading::PRICE) - sizeof(Trading::TIMESTAMP) - sizeof(Trading::QUANTITY) - sizeof(Trading::SYMBOL_ID) - sizeof(Side) - sizeof(OrderType)];
 
+  static Order Create(Trading::PRICE price, Trading::QUANTITY quantity, 
+                      Trading::SYMBOL_ID symbol_id, Side side, OrderType type,
+                      uint64_t id, Trading::TIMESTAMP timestamp) {
+
+    // perform value initalization to prevent garbage values left over in RAM 
+    Order order{};
+    order.id = id;
+    order.price = price;
+    order.timestamp = timestamp;
+    order.quantity = quantity;
+    order.symbol_id = symbol_id;
+    order.side = side;
+    order.type = type;
+    return order;
+    };
+};
